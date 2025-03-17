9b7eccd35e4dd19efd6578b3c7dc7c3715bbc7bf
Xiaotao Xu <xxttutd@gmail.com>
2025-03-17 13:45:46 -0400

    Hero image scaling

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const menuToggle = document.querySelector(".menu-toggle");
   const navList = document.querySelector(".nav-list");
 
-  if (menuToggle && navList) {
-    menuToggle.addEventListener("click", function () {
-      navList.classList.toggle("show");
-    });
-  }
+  menuToggle.addEventListener("click", function () {
+    navList.classList.toggle("show");
+  });
 });
